4f6c528a00ff979417f4bcc767e204028eb407ad
AristovaAlina <alinaaristova72@gmail.com>
2025-11-29 03:59:27 +0500

    Task3

---
+++ Tyuiu.AristovaAK.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AristovaAK.Sprint3.Task3.V21.Lib;
+namespace Tyuiu.AristovaAK.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceNumOnChar()
+        {
+            DataService ds = new DataService();
+            string value = "f3g5ht g4j 34kg4";
+            char item = 'e';
+            string wait = "fegeht gej eekge";
+            string res = ds.ReplaceNumOnChar(value, item);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AristovaAK.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AristovaAK.Sprint3.Task3.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #21                                                              *");
+        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach заменить цифры на букву e в                      *");
+        Console.WriteLine("* строке: f3g5ht g4j 34kg4                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string str = "f3g5ht g4j 34kg4";
+        char ch = 'e';
+        Console.WriteLine("Исходная строка: " + str);
+        Console.WriteLine("Искомый символ: " + ch);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Измененная строка: " + ds.ReplaceNumOnChar(str, ch));
+
+        Console.ReadKey();
+    }
+}
