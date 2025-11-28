2e2008d8670348c9da929a98b2c445841f42ccc2
AristovaAlina <alinaaristova72@gmail.com>
2025-11-29 01:38:06 +0500

    Task1

---
+++ Tyuiu.AristovaAK.Sprint3.Task1.V1.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AristovaAK.Sprint3.Task1.V1.Lib
+{
+    public class DataService : ISprint3Task1V1
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double res = 1;
+            while (startValue <= stopValue)
+            {
+                res *= Math.Pow((3.0 / startValue), -2);
+                startValue++;
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.AristovaAK.Sprint3.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AristovaAK.Sprint3.Task1.V1.Lib;
+namespace Tyuiu.AristovaAK.Sprint3.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int start = 1;
+            int stop = 7;
+            double wait = 5.311;
+            Assert.AreEqual(wait, ds.GetMultiplySeries(start, stop));
+        }
+    }
+}
