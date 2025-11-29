ee788b1133a69c9bfdc23940e6b7842d6d3d110f
AristovaAlina <alinaaristova72@gmail.com>
2025-11-29 06:06:40 +0500

    Task4

---
+++ Tyuiu.AristovaAK.Sprint3.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AristovaAK.Sprint3.Task4.V18.Lib;
+namespace Tyuiu.AristovaAK.Sprint3.Task4.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int start = -5;
+            int stop = 5;
+            double wait = 2.993;
+            double res = ds.Calculate(start, stop);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AristovaAK.Sprint3.Task4.V18/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AristovaAK.Sprint3.Task4.V18.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=sin(x)/cos(x). При х = 0 прервать цикл. Полученные значения   *");
+        Console.WriteLine("* суммировать.                                                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int start = -5;
+        int stop = 5;
+        Console.WriteLine("Начало отрезка = " + start);
+        Console.WriteLine("Конец отрезка = " + stop);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма значений = " + ds.Calculate(start, stop));
+
+        Console.ReadKey();
+    }
+}
