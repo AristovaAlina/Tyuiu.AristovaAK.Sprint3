f37f467b82a9dce89324cdeea1948192e8d22707
AristovaAlina <alinaaristova72@gmail.com>
2025-11-29 09:41:00 +0500

    Task7

---
+++ Tyuiu.AristovaAK.Sprint3.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.AristovaAK.Sprint3.Task7.V13.Lib;
+namespace Tyuiu.AristovaAK.Sprint3.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int start = -5;
+            int stop = 5;
+            int len = stop - start + 1;
+
+            double[] wait = new double[len];
+            wait[0] = -9.1;
+            wait[1] = 1.55; 
+            wait[2] = 292.78;
+            wait[3] = -0.57;
+            wait[4] = -0.35;
+            wait[5] = 2;
+            wait[6] = 4.35;
+            wait[7] = 4.57;
+            wait[8] = -288.78;
+            wait[9] = 2.45;
+            wait[10] = 13.1;
+
+            double[] res = new double[len];
+            res = ds.GetMassFunction(start, stop);
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AristovaAK.Sprint3.Task7.V13/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.AristovaAK.Sprint3.Task7.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции:           *");
+        Console.WriteLine("* F(x) = 3x + 3 - (2x - x)/(cos(x) + 1) (произвести табулирование) f(x)   *");
+        Console.WriteLine("* на заданном диапазоне [-5; 5] с шагом 1. Произвести проверку деления на *");
+        Console.WriteLine("* ноль. При делении на ноль вернуть 0. Значения занести в массив.         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int start = -5;
+        int stop = 5;
+        Console.WriteLine($"Числовой отрезок = [{start},{stop}]");
+
+        int len = ds.GetMassFunction(start, stop).Length;
+
+        double[] valueArray = new double[len];
+        valueArray = ds.GetMassFunction(start, stop);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("+---------+---------+");
+        Console.WriteLine("|    X    |   f(x)  |"); 
+        Console.WriteLine("+---------+---------+");
+        for (int i = 0; i <= len-1; i++)
+        {
+            Console.WriteLine("|{0,5:d}    |  {1, 5:f2}  |", start, valueArray[i]);
+            start++;
+        }
+
+        Console.WriteLine("+---------+---------+");
+        Console.ReadKey();
+    }
+}
